fc008b061580401464962d450862a7f191341ba7
Akanimoh Osutuk <akan@Akanimohs-MacBook-Pro.local>
2022-03-25 15:29:49 +0100

    working on the rewards contract

--- contracts/nftStaking/alphaRewards.sol
+++ contracts/nftStaking/alphaRewards.sol
@@ -2,3 +2,11 @@
 
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+
+/**
+ * @title Alpha Rewards
+ * @dev Calculates the rewards for staking on Alpha Shares
+ * @author Akanimoh Osutuk (docakan)
+ */
