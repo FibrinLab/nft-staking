17aabc3726c5bc6c06973c33c84dc21581297700
Akanimoh Osutuk <akan@Akanimohs-MacBook-Pro.local>
2022-03-25 15:20:41 +0100

    adding the rewards contract

--- contracts/nftStaking/alphaStaking.sol
+++ contracts/nftStaking/alphaStaking.sol
@@ -1,191 +1,331 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../interfaces/IALPHANFT.sol";
+import "../interfaces/IALPHARewards.sol";
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./NFT.sol";
-import "hardhat/console.sol";
+// To be implemented for security purposes
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract alphaStaking is Ownable {
-    using SafeMath for uint;
-    using SafeERC20 for IERC20;
+/**
+* @title Alpha NFT Staking Contract
+* @dev
+* @author Akanimoh Osutuk (DocAkan) 
+*/
 
-    struct Stake {
-        uint amount;
-        uint startTime; 
-        uint rewardOut;
+contract alphaStaking is AccessControl {
+    using SafeMath for uint256;
+
+    IERC20 public rewardsToken;
+    IALPHANFT public parentNFT;
+
+    // address public rewardsToken;
+    // NFT public parentNFT;
+
+    // Adopting the OpenZeppelin Implementation
+    // IERC721 public parentNFT;
+    IALPHARewards public rewardsContract;
+
+    bool initialised;
+    bool public tokensClaimable;
+
+    uint256 public stakedEthTotal;
+    uint256 public lastUpdateTime;
+    uint256 public powerLevel;
+
+    uint256 public rewardsPerTokenPoints;
+
+    uint256 constant pointMultiplier = 10e18;
+
+
+    /**
+    @notice Struct to track users and their tokens
+    @dev tokenIds are all the tokens staked by the staker
+    @dev balance is the current ether balance of the staker
+    @dev rewardsEarned is the total reward for the staker till now
+    @dev rewardsReleased is how much reward has been paid to the staker
+    */
+    struct Staker {
+        uint256[] tokenIds;
+        mapping (uint256 => uint256) tokenIndex;
+        uint256 balance;
+        uint256 lastRewardPoints;
+        uint256 rewardsEarned;
+        uint256 rewardsReleased;
     }
 
-    // Info of each pool.
-    struct Pool {
-        uint stakingCap; // Pool staking tokens limit
-        uint fixedStakingAmount; // Fixed staking amount to the pool
-        uint rewardAPY; // scaled by 1e12
-        uint startTime; 
-        uint endTime; 
-        uint stakedTotal; 
-    }
-    Pool[] public pools;
+    /// @notice Mapping of a staker to its properties
+    mapping(address => Staker) public stakers;
 
-    mapping(uint => mapping(address => Stake)) public stakes;
-    uint[] public nftIds;
-    mapping(address => uint[]) public nftIdsPerAddr;
-    address public stakeToken; // MRCH token
-    NFT public nft; // NFT token
+    /// @notice Mapping from token ID to the owner
+    mapping(uint256 => address) public tokenOwner;
 
-    uint randNonce = 0;
-    address public admin;
+    // Role identifier for the Admin Role
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-    // address public owner;
+    /// @notice Admin update of rewards contract
+    event RewardsTokenUpdated(address indexed oldRewardsToken, address newRewardsToken );
 
-    event Staked(uint pid, address staker, uint amount);
-    event RewardOut(uint pid, address staker, address token, uint amount);
+    /// @notice event emitted when a user has staked a token
+    event Staked(address owner, uint256 amount);
+
+    /// @notice event emitted when a user has unstaked a token
+    event Unstaked(address owner, uint256 amount);
+
+    /// @notice event emitted when a user claims reward
+    event RewardPaid(address user, uint256 amount);
+
+    /// @notice Emergency unstake tokens without rewards
+    event EmergencyUnstake(address indexed user, uint256 tokenId);
 
     constructor(
-        address _stakeToken,
-        address _nftAddress
+        // address _rewardsToken,
+        // address _parentNFT
     ) {
-        admin = msg.sender;
-        // setOwner(msg.sender);
-
-        require(_stakeToken != address(0), "alphaStaking: stake token address is 0");
-        stakeToken = _stakeToken;
-        nft = NFT(_nftAddress);
+        // require(rewardsToken != address(0), "MerchNStaking: stake token address is 0");
+        // rewardsToken = _rewardsToken;
+        // parentNFT = NFT(_parentNFT);
+        // grantRole(ADMIN_ROLE, msg.sender);
+        // parentNFT = IERC721(0x);
     }
 
-    // function setOwner(address _owner) public {
-    //     owner = _owner;
-    // }
-
-    // function getOwner() public view returns(address) {
-    //     return owner;
-    // }
+    /**
+    * @dev sets the admin role
+    */
+    function setAdmin(bytes32, address) external {
+        grantRole(ADMIN_ROLE, msg.sender);
+    }
     
-    function addPool(uint _stakingCap, uint _fixedStakingAmount, uint _rewardAPY, uint _startTime, uint _endTime) public onlyOwner {
-        require(_startTime < _endTime, "alphaStaking: endTime > startTime");
-        
-        pools.push(
-            Pool({
-            stakingCap: _stakingCap,
-            fixedStakingAmount: _fixedStakingAmount,
-            rewardAPY: _rewardAPY,
-            startTime: _startTime,
-            endTime: _endTime,
-            stakedTotal: 0
-            })
+    function isAdmin(bytes32, address) external view returns(bool) {
+        hasRole(ADMIN_ROLE, msg.sender);
+    }
+
+    /**
+    * @dev Init the staking contract
+    */
+
+    function initStaking(
+        IERC20 _rewardsToken,
+        IALPHANFT _parentNFT
+    ) external {
+        require(!initialised, "Already Initialised");
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller not an admin");
+        rewardsToken = _rewardsToken;
+        parentNFT = _parentNFT;
+        lastUpdateTime = block.timestamp;
+        initialised = true;
+    }
+
+
+    function setRewardsContract(
+        address _addr
+    )
+        external
+    {
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller must be admin");
+        require(_addr != address(0));
+        address oldaddr = address(rewardsContract);
+        rewardsContract = IALPHARewards(_addr);
+        emit RewardsTokenUpdated(oldaddr, _addr);
+    }
+
+
+    // @dev Getter functions for the staking contract
+    // @dev Gets the amount of tokens staked by the user
+    function getStakedTokens(
+        address _user
+    )
+        external
+        view
+        returns (uint256[] memory tokenIds)
+    {
+        return stakers[_user].tokenIds;
+    }
+
+
+    /// @notice Stake ALPHA NFTS and earn reward tokens
+    function stake(
+        uint256 tokenId
+    )
+        external
+    {
+        _stake(msg.sender, tokenId);
+    }
+
+    function _stake(
+        address _user,
+        uint256 _tokenId
+    )
+        internal
+    {
+        Staker storage staker = stakers[_user];
+
+        if (staker.balance == 0 && staker.lastRewardPoints == 0 ) {
+            staker.lastRewardPoints = rewardsPerTokenPoints;
+        }
+
+        updateReward(_user);
+        uint256 amount = getContribution(_tokenId);
+        staker.balance = staker.balance.add(amount);
+        stakedEthTotal = stakedEthTotal.add(amount);
+        staker.tokenIds.push(_tokenId);
+        staker.tokenIndex[staker.tokenIds.length - 1];
+        tokenOwner[_tokenId] = _user;
+        parentNFT.safeTransferFrom(
+            _user,
+            address(this),
+            _tokenId
         );
+
+        emit Staked(_user, _tokenId);
     }
 
-    function stake(uint _pid, uint _amount) public returns (bool) {
-        address staker = msg.sender;
-
-        require(_amount == pools[_pid].fixedStakingAmount, "alphaStaking: wrong amount");
-        require(stakes[_pid][staker].amount == 0, "alphaStaking: already staked");
-        require(getTimeStamp() >= pools[_pid].startTime, "alphaStaking: bad timing request");
-        require(getTimeStamp() < pools[_pid].endTime, "alphaStaking: bad timing request");
-
-        require(pools[_pid].stakedTotal.add(_amount) <= pools[_pid].stakingCap, "alphaStaking: Staking cap is filled");
-    
-        transferIn(staker, stakeToken, _amount);
-
-        emit Staked(_pid, staker, _amount);
-
-        // Transfer is completed
-        pools[_pid].stakedTotal = pools[_pid].stakedTotal.add(_amount);
-        stakes[_pid][staker].amount = stakes[_pid][staker].amount.add(_amount);
-        stakes[_pid][staker].startTime = getTimeStamp();
-        stakes[_pid][staker].rewardOut = 0;
-
-        return true;
+    /// @notice Unstake ALPHA NFTs
+    function unstake(
+        uint256 _tokenId
+    )
+        external
+    {
+        require(tokenOwner[_tokenId] == msg.sender, "Sender must have staked tokenIDs");
+        claimReward(msg.sender);
+        _unstake(msg.sender, _tokenId);
     }
 
-    function withdraw(uint _pid) public returns (bool) {
-        uint amount = stakes[_pid][msg.sender].amount;
-        require(getTimeStamp() > pools[_pid].endTime, "alphaStaking: bad timing request");
-        require(amount > 0, "alphaStaking: nothing to withdraw");
-        require(claim(_pid), "alphaStaking: claim error");
 
-        genNFTId(msg.sender);
+    /**
+     * @dev All the unstaking goes through this function
+     * @dev Rewards to be given out is calculated
+     * @dev Balance of stakers are updated as they unstake the nfts based on ether price
+     */
+    function _unstake(
+        address _user, 
+        uint256 _tokenId
+    )
+        internal
+    {
+        Staker storage staker = stakers[_user];
+        uint256 amount = getContribution(_tokenId);
+        staker.balance = staker.balance.sub(amount);
+        stakedEthTotal = stakedEthTotal.sub(amount);
 
-        return withdrawWithoutReward(_pid, amount);
-    }
+        uint256 lastIndex = staker.tokenIds.length - 1;
+        uint256 lastIndexKey = staker.tokenIds[lastIndex];
+        uint256 tokenIdIndex = staker.tokenIndex[_tokenId];
 
-    function genNFTId(address _to) internal {
-        uint newNFTId = rand();
-        nftIds.push(newNFTId);
-        nftIdsPerAddr[_to].push(newNFTId);
-        nft.mint(_to, newNFTId);
-    }
-
-    function withdrawWithoutReward(uint _pid, uint _amount) public returns (bool) {
-        return withdrawInternal(_pid, msg.sender, _amount);
-    }
-
-    function withdrawInternal(uint _pid, address _staker, uint _amount) internal returns (bool) {
-        require(_amount > 0, "alphaStaking: must be positive");
-        require(_amount <= stakes[_pid][msg.sender].amount, "alphaStaking: not enough balance");
-
-        stakes[_pid][_staker].amount = stakes[_pid][_staker].amount.sub(_amount);
-
-        transferOut(stakeToken, _staker, _amount);
-
-        return true;
-    }
-
-    function claim(uint _pid) internal returns (bool) {
-        require(getTimeStamp() > pools[_pid].endTime, "alphaStaking: bad timing request");
-        address staker = msg.sender;
-        
-        uint rewardAmount = currentReward(_pid, staker);
-        if (rewardAmount == 0) {
-            return true;
+        staker.tokenIds[tokenIdIndex] = lastIndexKey;
+        staker.tokenIndex[lastIndexKey] = tokenIdIndex;
+        if (staker.tokenIds.length > 0) {
+            staker.tokenIds.pop();
+            delete staker.tokenIndex[_tokenId];
         }
 
-        transferOut(stakeToken, staker, rewardAmount);
+        if (staker.balance == 0) {
+            delete stakers[_user];
+        }
+        delete tokenOwner[_tokenId];
 
-        stakes[_pid][staker].rewardOut = stakes[_pid][staker].rewardOut.add(rewardAmount);
+        parentNFT.safeTransferFrom(
+            address(this), 
+            _user, 
+            _tokenId
+        );
 
-        emit RewardOut(_pid, staker, stakeToken, rewardAmount);
-
-        return true;
+        emit Unstaked(_user, _tokenId);
     }
 
-    function currentReward(uint _pid, address _staker) public view returns (uint) {
-        uint totalRewardAmount = stakes[_pid][_staker].amount.mul(pools[_pid].rewardAPY).div(1e12).div(100);
-        uint totalDuration = pools[_pid].endTime - stakes[_pid][_staker].startTime;
-        uint duration = (getTimeStamp() > pools[_pid].endTime ? pools[_pid].endTime : getTimeStamp()) - stakes[_pid][_staker].startTime;
+    /// @dev Updates the amount of rewards owed for each user before any tokens are moved
+    function updateReward(
+        address _user
+    )
+        public
+    {
+        rewardsContract.updateRewards();
+        /// Review and update the interface contract
+        uint256 parentRewards = rewardsContract.parentRewards(lastUpdateTime, block.timestamp);
 
-        uint rewardAmount = totalRewardAmount.mul(duration).div(totalDuration);
-        return rewardAmount.sub(stakes[_pid][_staker].rewardOut);
-    }
-
-    function transferOut(address _token, address _to, uint _amount) internal {
-        if (_amount == 0) {
-            return;
+        if (stakedEthTotal > 0) {
+            rewardsPerTokenPoints = rewardsPerTokenPoints.add(parentRewards
+            .mul(1e18)
+            .mul(pointMultiplier)
+            .div(stakedEthTotal));
         }
 
-        IERC20 ERC20Interface = IERC20(_token);
-        ERC20Interface.safeTransfer(_to, _amount);
+        lastUpdateTime = block.timestamp;
+        uint256 rewards = rewardsDue(_user);
+
+        Staker storage staker = stakers[_user];
+        if (_user != address(0)) {
+            staker.rewardsEarned = staker.rewardsEarned.add(rewards);
+            staker.lastRewardPoints = rewardsPerTokenPoints; 
+        }
     }
 
-    function transferIn(address _from, address _token, uint _amount) internal {
-        IERC20 ERC20Interface = IERC20(_token);
-        ERC20Interface.safeTransferFrom(_from, address(this), _amount);
+
+    /// @notice Returns the rewards due for a user
+    /// @dev This gets the rewards from each of the periods as one multiplier
+    function rewardsDue(
+        address _user
+    )
+        public
+        view
+        returns(uint256)
+    {
+        uint256 newRewardPerToken = rewardsPerTokenPoints.sub(stakers[_user].lastRewardPoints);
+        uint256 rewards = stakers[_user].balance.mul(newRewardPerToken)
+            .div(1e18)
+            .div(pointMultiplier);
     }
 
-    function getTimeStamp() public view virtual returns (uint) {
-        return block.timestamp;
+    /// @dev gets the value of a staked NFT
+    function getContribution(
+        uint256 _tokenId
+    )
+        public
+        view
+        returns (uint256)
+    {
+        // return parentNFT.basicSalePrice(_tokenId);
     }
 
-    function rand() internal view returns(uint) {
-        return randMod(2 ** 16);
-        
+
+    function claimReward(
+        address _user
+    )
+        public
+    {
+        require(tokensClaimable == true, "Cannot be claimed yet");
+        updateReward(_user);
+
+        Staker storage staker = stakers[_user];
+
+        uint256 payableAmount = staker.rewardsEarned.sub(staker.rewardsReleased);
+        staker.rewardsReleased = staker.rewardsReleased.add(payableAmount);
+
+        /// @dev 
+        uint256 rewardBal = rewardsToken.balanceOf(address(this));
+        if (payableAmount > rewardBal) {
+            payableAmount = rewardBal;
+        }
+
+        rewardsToken.transfer(_user, payableAmount);
+        emit RewardPaid(_user, payableAmount);
     }
 
-    function randMod(uint _modulus) internal view returns(uint) {
-        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % _modulus;
+
+    // Unstake without caring about rewards. EMERGENCY ONLY.
+    function emergencyUnstake(uint256 _tokenId) public {
+        require(
+            tokenOwner[_tokenId] == msg.sender,
+            "Sender must have staked tokenID"
+        );
+        _unstake(msg.sender, _tokenId);
+        emit EmergencyUnstake(msg.sender, _tokenId);
+
     }
+
 }
